b0c25e0bdf00676fc240016dbce42a0d6d82a269
Tom Middlehurst <tom.middlehurst@hotmail.co.uk>
2017-01-30 11:50:12 +0000

    First commit, HTML and CSS layout is complete, starting to add JS now

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.getElementById("game-area").innerHTML = "Hello World!";
+
+$(document).ready(function(){
+	$("#timer-and-scores").click(function(){
+	    $("#timer-and-scores").hide();
+	});
+
+	$("#timer-and-scores").click(function(){
+	    $("#timer-and-scores").show();
+	});
+})
